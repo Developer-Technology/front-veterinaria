809f7261a0c026d9b82ec069f273a20429421817
chanamoth <mario.rojas.chanamoth@gmail.com>
2024-10-09 20:17:37 -0500

    Login

--- src/app/views/pages/auth/login/login.component.ts
+++ src/app/views/pages/auth/login/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent implements OnInit {
   email: string = '';
   password: string = '';
   errorMessage: string = '';
-
+  isLoading: boolean = false;
   returnUrl: any;
 
   constructor(private apiService: ApiService, private router: Router, private route: ActivatedRoute) { }
@@ -22,14 +22,17 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit() {
+    this.isLoading = true;
     this.apiService.post('api/auth/login', { email: this.email, password: this.password }).subscribe(
       (response) => {
         localStorage.setItem('isLoggedin', 'true');
         this.router.navigate([this.returnUrl]);
         localStorage.setItem('token', response.access_token); // Guardar el token
         this.router.navigate(['/dashboard']); // Redirigir al dashboard
+        this.isLoading = false;
       },
       (error) => {
+        this.isLoading = false;
         this.errorMessage = 'Invalid email or password'; // Mostrar mensaje de error
       }
     );
