f269be913dab6a6046b5e0f798b54ab91ea4cc43
chanamoth <mario.rojas.chanamoth@gmail.com>
2024-10-09 19:24:50 -0500

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  URL_BACKEND: 'https://api-megarestaurant.chanamoth.com/',
+  URL_SERVICIOS: 'https://api-megarestaurant.chanamoth.com/api',
+  URL_FRONTED: 'https://megarestaurant.chanamoth.com',
+};
