dc0de910df4e25b5537513f3c54bef60a37233b7
chanamoth <mario.rojas.chanamoth@gmail.com>
2024-10-14 21:08:44 -0500

    Se corrige foto null en perfil de mascota

--- src/app/shared/custom-field/custom-field.component.ts
+++ src/app/shared/custom-field/custom-field.component.ts
@@ -9,4 +9,9 @@ export class CustomFieldComponent {
   @Input() imageUrl?: string = '';  // URL de la imagen
   @Input() title: string = '';     // Título (e.g., nombre de la mascota)
   @Input() subtitle: string = '';  // Subtítulo (e.g., especie y raza)
+
+  get validImageUrl(): string {
+    return this.imageUrl ? this.imageUrl : 'assets/images/default/blank-pet.png';
+  }
+  
 }
